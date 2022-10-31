5f0436405d78a436d7bd9aa8bbefdcc5d890dc7c
ThomasCisneroz <thomasrcisnseroz@gmail.com>
2022-10-31 14:27:39 -0400

    registering user

---
+++ backend/models/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "please add a name"]
+    },
+    email: {
+        type: String,
+        required: [true, 'please add an email'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'please provide a password']
+    },
+    isAdmin: {
+        type: Boolean,
+        required: true,
+        default: false
+    }
+},
+{
+    timestamps: true,
+}
+)
+
+module.exports = mongoose.model('User', userSchema);
